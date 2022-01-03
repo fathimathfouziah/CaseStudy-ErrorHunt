d6b09a579bfa21d54bce91271740836d007dbd9f
fathimathfouziah <93689605+fathimathfouziah@users.noreply.github.com>
2022-01-03 15:45:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -70,6 +70,6 @@ app.get('/',function(req,res){
 
 
 
-app.listen(5000,()=>{
+var server = app.listen(5000,()=>{
     console.log("Server Ready on 5000");// console.log("Server Ready on 3000"); were given
 });
